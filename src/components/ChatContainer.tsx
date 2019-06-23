d3af47c237268b4bc3ded78a591db354ff2bdf83
nowheyjose <jdiaz5511@gmail.com>
2019-06-22 20:22:32 -0400

    resolved issues

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -6,6 +6,7 @@ interface ChatProps {
     logout: () => void
 }
 
+// chat container not rendering upon login need to fix
 export class ChatContainer extends React.Component<ChatProps> {
     public render() {
         return <div>Chat Container</div>
